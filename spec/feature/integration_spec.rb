af905b1d61bb02ed42087997b5f0974d3e049e77
LanceAbuan <69011514+LanceAbuan@users.noreply.github.com>
2022-02-13 02:10:43 -0600

    Beginning Rspec
    Added testing for some tables, still a work in progress as I'm hung up on working on some form autofills

---
+++ spec/feature/integration_spec.rb
@@ -0,0 +1,54 @@
+# location: spec/feature/integration_spec.rb
+require 'rails_helper'
+
+RSpec.describe 'Creating a tamu department', type: :feature do
+  scenario 'valid inputs' do
+    visit new_tamu_department_path
+    fill_in 'tamu_department_tamu_department_name', with: 'CSCE'
+    click_on 'Create Tamu department'
+    visit tamu_departments_path
+    expect(page).to have_content('CSCE')
+  end
+end	
+
+RSpec.describe 'Creating a University', type: :feature do
+  scenario 'valid inputs' do
+    visit new_university_path
+    fill_in 'university_country', with: 'United Kingdom'
+    fill_in 'university_university_name', with: 'Oxford'
+    click_on 'Create University'
+    visit universities_path
+    expect(page).to have_content('United Kingdom')
+  end
+end	
+
+RSpec.describe 'Creating a TAMU Course', type: :feature do
+  scenario 'valid inputs' do
+	visit new_tamu_department_path
+    fill_in 'tamu_department_tamu_department_name', with: 'CSCE'
+    click_on 'Create Tamu department'
+    visit tamu_departments_path 
+  
+    visit new_tamu_course_path
+    fill_in 'tamu_course_course_num', with: 20
+    fill_in 'tamu_course_tamu_department_id', with: 0
+    click_on 'Create Tamu course'
+    visit tamu_courses_path
+    expect(page).to have_content('CSCE')
+  end
+end	
+
+
+RSpec.describe 'Creating a foreign course', type: :feature do
+  scenario 'valid inputs' do
+    visit new_foreign_course_path
+    fill_in 'Instrutor', with: 'harry potter'
+	fill_in 'foreign_course_foreign_course_name', with: 'Design Algo'
+	fill_in 'foreign_course_credit_hours', with: 10
+	fill_in 'foreign_course_semester_approved', with: 'Fall 2020'
+	fill_in 'foreign_course_tamu_department_id', with: 10
+    click_on 'Create Foreign course'
+    visit foreign_courses_path
+    expect(page).to have_content('harry potter')
+  end
+end	
