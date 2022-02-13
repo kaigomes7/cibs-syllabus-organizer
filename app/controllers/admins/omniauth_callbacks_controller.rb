b140114743eeb59dc81534596f78787942a5b941
Zachary Lawton <zlaw627@gmail.com>
2022-02-13 11:39:23 -0600

    oauth should be done but testing later

--- app/controllers/admins/omniauth_callbacks_controller.rb
+++ app/controllers/admins/omniauth_callbacks_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Admins::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     def google_oauth2
       admin = Admin.from_google(from_google_params)
--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,12 +1,8 @@
 class Admin < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :omniauthable, omniauth_providers: [:google_oauth2]
-  # devise :database_authenticatable, :registerable,
-  #        :recoverable, :rememberable, :validatable
-  
+
   def self.from_google(email:, full_name:, uid:, avatar_url:)
-    return nil unless email =~ /@tamu.edu\z/
+    return nil unless email =~ /@gmail.com || @tamu.edu\z/
     create_with(uid: uid, full_name: full_name, avatar_url: avatar_url).find_or_create_by!(email: email)
   end
 end
