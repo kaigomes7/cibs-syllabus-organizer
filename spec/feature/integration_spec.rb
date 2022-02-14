6e5164426bae54d70d4cd56cf4da3c6daf2f5a66
LanceAbuan <69011514+LanceAbuan@users.noreply.github.com>
2022-02-13 21:53:55 -0600

    Dropdown Menu
    Populating a dropdown menu from a table is a WIP but I can't remember/figure out how to do it for some reason, so I'm gonna ask someone to work on it eventually when I get to see them in person.

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -21,19 +21,22 @@ RSpec.describe 'Creating a University', type: :feature do
     expect(page).to have_content('United Kingdom')
   end
 end	
-'''
+
+=begin
 RSpec.describe 'Creating a TAMU Course', type: :feature do
   scenario 'valid inputs' do
 	visit new_tamu_department_path
     fill_in 'tamu_department_tamu_department_name', with: 'CSCE'
     click_on 'Create Tamu department'
+    visit tamu_departments_path
+    expect(page).to have_content('CSCE')
   
     visit new_tamu_course_path
-    fill_in 'tamu_course_course_num', with: 20
+    fill_in 'tamu_course_course_num', with: 5
     fill_in 'tamu_course_tamu_department_id', with: 1
     click_on 'Create Tamu course'
     visit tamu_courses_path
-    expect(page).to have_content(1)
+    expect(page).to have_content('1')
   end
 end	
 
@@ -51,5 +54,6 @@ RSpec.describe 'Creating a foreign course', type: :feature do
     visit foreign_courses_path
     expect(page).to have_content('harry potter')
   end
-'''
 end	
+
+=end
