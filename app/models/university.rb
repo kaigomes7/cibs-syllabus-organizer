b5a79491c15374094e0352d8c07203a5f0e8f84b
Kai Gomes <kaigomes7@tamu.edu>
2022-02-14 21:57:13 -0600

    fixing github actions #3

--- app/models/university.rb
+++ app/models/university.rb
@@ -1,10 +1,6 @@
 # frozen_string_literal: true
 
 class University < ApplicationRecord
-<<<<<<< HEAD
-  has_many :foreign_course
-=======
-    validates :country, :university_name, presence: true
+  validates :country, :university_name, presence: true
 	has_many :foreign_course
->>>>>>> lance-rspec
 end
