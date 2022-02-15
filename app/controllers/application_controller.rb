cbd297bcdd9e2c66d81956930ed635a6d77d7778
Kai Gomes <kaigomes7@tamu.edu>
2022-02-14 20:24:10 -0600

    Revert "Merge branch 'zach-oauth' into test-review-app"
    This reverts commit 7b146eda99988aa8bcddaaf61526c9895e3661a8, reversing
    changes made to 79568ef1d42c1f86a3ba93c869116c6444884555.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,2 @@
 class ApplicationController < ActionController::Base
-    before_action :authenticate_admin!
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  # devise_for :admins
   resources :foreign_courses_tamu_courses
   resources :reviewers
   resources :tamu_departments
@@ -8,16 +7,8 @@ Rails.application.routes.draw do
   resources :students
   resources :foreign_courses
   resources :courses_students
-  # root "pending_requests#index"
+  root "pending_requests#index"
   get "pending_requests", to: "pending_requests#index"
   get "all_requests", to: "all_requests#index"
   get "approved_requests", to: "approved_requests#index"
-  # root "reviewers#index"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'dashboards#show'
-  devise_for :admins, controllers: { omniauth_callbacks: 'admins/omniauth_callbacks' }
-  devise_scope :admin do
-    get 'admins/sign_in', to: 'admins/sessions#new', as: :new_admin_session
-    get 'admins/sign_out', to: 'admins/sessions#destroy', as: :destroy_admin_session
-  end
 end
