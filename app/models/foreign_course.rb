899ddcc6a7eee80edd530558090932bec5f7b094
Kai Gomes <kaigomes7@tamu.edu>
2022-02-14 22:15:37 -0600

    Fixing github actions #6

--- app/models/foreign_course.rb
+++ app/models/foreign_course.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ForeignCourse < ApplicationRecord
-  validates :instrutor, :foreign_course_name, :credit_hours, :semester_approved, presence: true
+  validates :instructor, :foreign_course_name, :credit_hours, :semester_approved, presence: true
   belongs_to :tamu_department
   belongs_to :university
   has_and_belongs_to_many :student
