b81a03a512e5ab36ce7683548c36383d721401a2
LanceAbuan <69011514+LanceAbuan@users.noreply.github.com>
2022-02-14 18:33:28 -0600

    Unit Test Set 1
    Created unit tests for all variables in set 1. Set 1 consists of universities and tamu_departments

--- spec/unit/unit_spec.rb
+++ spec/unit/unit_spec.rb
@@ -1,18 +1,50 @@
 # location: spec/unit/unit_spec.rb
 require 'rails_helper'
 
-RSpec.describe CoursesStudent, type: :model do
+RSpec.describe University, type: :model do
   subject do
-    described_class.new(student_id: 1)
-    described_class.new(foreign_course_id: 1)
+    described_class.new(country: 'USA', university_name: 'Harvard')
+	
   end
 
   it 'is valid with valid attributes' do
     expect(subject).to be_valid
   end
 
-  it 'is not valid without a name' do
-    subject.student_id = nil
-    expect(subject).no	t_to be_valid
+  it 'is not valid without a country' do
+    subject.country = nil
+    expect(subject).not_to be_valid
+  end
+end
+
+RSpec.describe University, type: :model do
+  subject do
+    described_class.new(country: 'USA', university_name: 'Harvard')
+	
+  end
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid without a university_name' do
+    subject.university_name = nil
+    expect(subject).not_to be_valid
+  end
+end
+
+RSpec.describe TamuDepartment, type: :model do
+  subject do
+    described_class.new(tamu_department_name: 'CSCE')
+	
+  end
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid without a country' do
+    subject.tamu_department_name = nil
+    expect(subject).not_to be_valid
   end
 end
