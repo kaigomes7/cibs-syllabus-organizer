0ce889bdf20ce986804f0cb7ee4c9648c597bf1b
tdepaulis <trinitydepaulis@tamu.edu>
2022-03-04 02:39:49 -0600

    added tables to two additional pages and fixed errors with controllers

--- app/controllers/all_course_requests_controller.rb
+++ app/controllers/all_course_requests_controller.rb
@@ -3,5 +3,8 @@ class AllCourseRequestsController < ApplicationController
         @foreign_courses = ForeignCourse.all
         @tamu_departments = TamuDepartment.all
         @universities = University.all
+        @reviewers = Reviewer.all
+        @tamu_courses = TamuCourse.all
+        @students = Student.all
     end   
 end
--- app/controllers/approved_requests_reviewers_controller.rb
+++ app/controllers/approved_requests_reviewers_controller.rb
@@ -1,3 +1,10 @@
 class ApprovedRequestsReviewersController < ApplicationController
-    def index; end
+    def index
+        @foreign_courses = ForeignCourse.all
+        @tamu_departments = TamuDepartment.all
+        @universities = University.all
+        @reviewers = Reviewer.all
+        @tamu_courses = TamuCourse.all
+        @students = Student.all
+    end   
 end
