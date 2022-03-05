70c4d7c0179b05f7cbc709342c87f8f4ab9ebe91
Kai Gomes <kaigomes7@tamu.edu>
2022-03-04 20:55:13 -0600

    Fixed broken delete (someone broke jquery)

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,7 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "bootstrap"
-import "../js/bootstrap_js_files.js"
+// import "../js/bootstrap_js_files.js"
 
 Rails.start()
 Turbolinks.start()
