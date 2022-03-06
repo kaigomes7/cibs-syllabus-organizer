3a1f04823d624ce8a8aa3a86c771d4ad58732b50
tdepaulis <trinitydepaulis@tamu.edu>
2022-03-06 14:50:00 -0600

    minor changes to pending reviewer apporved -- testing something out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,27 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+    def is_logged_in?
+        !!session[:user_id]
+    end
+
+    def is_student?
+        5 > 1
+        # if boolean in roles table is null for this specific uin/user id
+        # return true
+        # else return false
+    end
+
+    def is_admin?
+        5 > 1
+    end
+
+    def is_reviewer?
+        5 > 1
+    end
+
+    # who the current user is
+    def current_user
+        @current_user || User.find(session[:user_id]) if is_logged_in?
+    end 
 end
