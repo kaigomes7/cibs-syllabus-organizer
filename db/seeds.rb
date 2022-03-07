06a1037ae9570343ddb7f3f9990706d981ef46d2
Kai Gomes <kaigomes7@tamu.edu>
2022-03-06 18:07:11 -0600

    Fixed destroy routes for bridge tables and user table

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,9 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 TamuDepartment.create(tamu_department_name: "FINC")
-
-University.create(university_name: "Monster's University", country: "Moon")
-
-Student.create(student_email: "js@js.com", student_name: "jon smith", tamu_department: 1)
+University.create(university_name: "Monster's University", city_country: "Moon")
+User.create(email: "js@js.com", name: "jon smith")
+Student.create(user_id: 1, tamu_department_id: 1, tamu_major: 'CSCE', tamu_college: 'Engineering', classification: 'Senior')
+TamuCourse.create(tamu_department_id: 1, course_num: 143, course_name: 'FINC 143')
+ForeignCourse.create(tamu_department_id: 1, foreign_course_name: 'Intro to Finance', contact_hours: 45, university_id: 1, semester_approved: 'Spring 2020', course_approval_status: false, foreign_course_dept: 'FNCC', foreign_course_num: '1234')
+ForeignCoursesStudent.create(student_id: 1, )
