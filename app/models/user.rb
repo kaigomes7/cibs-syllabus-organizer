c34808ac164d3eaf969b0ebe647b3307f7145093
root <root@localhost>
2022-03-08 02:47:19 +0000

    Testing Oauth for Heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
     devise :omniauthable, omniauth_providers: [:google_oauth2]
     
     def self.from_google(email:, full_name:, uid:)
-        create_with(uid: uid, name: full_name).find_or_create_by!(uid: uid)
+        create_with(uid: uid, email: email, name: full_name).find_or_create_by!(uid: uid)
     end
 end
