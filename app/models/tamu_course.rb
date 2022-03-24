272a3157475fccbda8a9eed57a2bd83966c1c37e
Kai Gomes <kaigomes7@tamu.edu>
2022-03-24 02:35:40 -0500

    Removed uneeded change

--- app/models/tamu_course.rb
+++ app/models/tamu_course.rb
@@ -1,9 +1,5 @@
 class TamuCourse < ApplicationRecord
   validates :course_num, :course_name, presence: true
   belongs_to :tamu_department
-  has_and_belongs_to_many :foreign_course
-
-  def formatted_course
-    "#{TamuDepartment.find(tamu_department_id).tamu_department_name} #{course_num}"
-  end
+  # has_and_belongs_to_many :foreign_course
 end
