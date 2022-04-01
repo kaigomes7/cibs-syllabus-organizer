4c2e17ff37aa856f162504d777e91c28cedf1149
tdepaulis <trinitydepaulis@tamu.edu>
2022-03-31 21:38:29 -0500

    updated pending reviewer approved with buttons and a way to update if something is approved or not.

--- app/controllers/pending_reviewer_approved_controller.rb
+++ app/controllers/pending_reviewer_approved_controller.rb
@@ -1,5 +1,6 @@
 class PendingReviewerApprovedController < ApplicationController
     def index
         @foreign_courses = ForeignCourse.where(course_approval_status: true)
+        @foreign_courses_students = ForeignCoursesStudent.all
     end   
 end
