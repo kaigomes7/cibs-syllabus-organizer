dcd4dc5f561eba7c64dece780ca76cbad23ae03f
vitruong00 <vimtruong00@gmail.com>
2022-04-07 01:52:52 -0500

    Fixed bugs where admin change role for existing student or reviewer

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -10,7 +10,16 @@ class UserController < ApplicationController
         redirect_to syllabi_reviewer_url if current_user.role == 2
         @user = User.find(params[:id])
         if @user.update(user_params)
-          redirect_to action: "index"
+            redirect_to action: "index"
+            @student = Student.find_by_user_id(@user.id)
+            if @student != nil
+                @student.destroy
+            end
+            @reviewer = Reviewer.find_by_user_id(@user.id)
+            if @reviewer != nil
+                @reviewer.destroy
+            end
+            
         else
           flash[:errors] = @user.errors.full_messages
           redirect_to edit_user_path
