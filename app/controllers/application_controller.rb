358a8b60446f1d58cd311a8fdc8b84eb2a5362c5
tdepaulis <trinitydepaulis@tamu.edu>
2022-04-06 22:51:02 -0500

    fixed duplicate logic and implemented the update for all logic. Fixed the pending reviewer approved page. Removed the edit button for all except pending requests. Updated the application controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
         if Rails.env == 'test'
             return ENV['TEST_USER'] == 'reviewer'
         else
-            return Reviewer.exists?(user_id: current_user.id)
+            return User.find_by_id(current_user.id).role == 2
         end
     end
 
@@ -15,7 +15,7 @@ class ApplicationController < ActionController::Base
         if Rails.env == 'test'
             return ENV['TEST_USER'] == 'admin'
         else
-            return Admin.exists?(user_id: current_user.id)
+            return User.find_by_id(current_user.id).role == 0
         end
     end
 
@@ -23,7 +23,7 @@ class ApplicationController < ActionController::Base
         if Rails.env == 'test'
             return ENV['TEST_USER'] == 'student'
         else
-            return Student.exists?(user_id: current_user.id)
+            return User.find_by_id(current_user.id).role == 1
         end
     end
 end
--- app/controllers/foreign_courses_controller.rb
+++ app/controllers/foreign_courses_controller.rb
@@ -43,32 +43,47 @@ class ForeignCoursesController < ApplicationController
     @reviewer = reviewer?
     start_dates = foreign_course_params.slice!("start_date(1i)", "start_date(2i)", "start_date(3i)")
     end_dates = foreign_course_params.slice!("end_date(1i)", "end_date(2i)", "end_date(3i)")
-    new_params = foreign_course_params.slice!("foreign_course_name", "contact_hours", "semester_approved", "tamu_department_id", "university_id", "foreign_course_num", "foreign_course_dept", "course_approval_status", "syllabus")
-    @foreign_course = ForeignCourse.new(new_params)
-    if @foreign_course.course_approval_status.nil?
-      @foreign_course.course_approval_status = false
-    end
-    if @foreign_course.contact_hours.nil?
-      @foreign_course.contact_hours = 0
-    end
-    if @foreign_course.tamu_department_id.nil?
-      @foreign_course.tamu_department_id = TamuDepartment.find_by(tamu_department_name: "Unassigned").id
-    end
+    
+    check_dup_parms = foreign_course_params.slice( "university_id", "foreign_course_num", "foreign_course_dept")
+    temp = ForeignCourse.find_by(university_id: check_dup_parms["university_id"], foreign_course_num: check_dup_parms["foreign_course_num"], foreign_course_dept: check_dup_parms["foreign_course_dept"])
 
+    # if the course exists and the FC is approved (approval from Reviewer)
+    dup = false
+    if temp && temp.course_approval_status == true
+      dup = true
+      @foreign_course = temp
+      puts "in if"
+    else
+      new_params = foreign_course_params.slice!("foreign_course_name", "contact_hours", "semester_approved", "tamu_department_id", "university_id", "foreign_course_num", "foreign_course_dept", "course_approval_status", "syllabus")
+      @foreign_course = ForeignCourse.new(new_params)
+      if @foreign_course.course_approval_status.nil?
+        @foreign_course.course_approval_status = false
+      end
+      if @foreign_course.contact_hours.nil?
+        @foreign_course.contact_hours = 0
+      end
+      if @foreign_course.tamu_department_id.nil?
+        @foreign_course.tamu_department_id = TamuDepartment.find_by(tamu_department_name: "Unassigned").id
+      end
+      puts "in else"
+    end
     respond_to do |format|
-      if @foreign_course.save
-        format.html { redirect_to foreign_course_url(@foreign_course), notice: "Foreign course was successfully created." }
-        format.json { render :show, status: :created, location: @foreign_course }
-        
-        #create join-table entry if foreign_course succeeds
-        sd = start_dates["start_date(1i)"] + "-" + start_dates["start_date(2i)"] + "-" + start_dates["start_date(3i)"]
-        ed = end_dates["end_date(1i)"] + "-" + end_dates["end_date(2i)"] + "-" + end_dates["end_date(3i)"]
-        curr_student = (Rails.env == 'test') ? 1 : Student.find_by(user_id: current_user.id).id
-        @foreign_course_student = ForeignCoursesStudent.new(foreign_course_id: @foreign_course.id,
-          student_id: curr_student,
-          start_date: Date.parse(sd, '%Y-%m-%d'),
-          end_date: Date.parse(ed, '%Y-%m-%d'), admin_course_approval: false)
-        @foreign_course_student.save
+      if dup || @foreign_course.save
+        format.html { redirect_to my_requests_path}
+        student = Student.find_by(user_id: current_user.id).id
+        #check foreign course student table
+        temp_2 = ForeignCoursesStudent.find_by(student_id: student, foreign_course_id: @foreign_course.id)
+        if !temp_2 || temp_2.admin_course_approval == nil || @foreign_course.course_approval_status == nil
+          #create join-table entry if foreign_course succeeds
+          sd = start_dates["start_date(1i)"] + "-" + start_dates["start_date(2i)"] + "-" + start_dates["start_date(3i)"]
+          ed = end_dates["end_date(1i)"] + "-" + end_dates["end_date(2i)"] + "-" + end_dates["end_date(3i)"]
+          curr_student = (Rails.env == 'test') ? 1 : student
+          @foreign_course_student = ForeignCoursesStudent.new(foreign_course_id: @foreign_course.id,
+            student_id: curr_student,
+            start_date: Date.parse(sd, '%Y-%m-%d'),
+            end_date: Date.parse(ed, '%Y-%m-%d'), admin_course_approval: false)
+          @foreign_course_student.save
+        end
       else
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @foreign_course.errors, status: :unprocessable_entity }
@@ -85,6 +100,16 @@ class ForeignCoursesController < ApplicationController
         # create tamu course connection if approved
         if @foreign_course.course_approval_status
           @foreign_course_tamu_course = ForeignCoursesTamuCourse.new(foreign_course_id: @foreign_course.id, tamu_course_id: tamu_course_map['tamu_course_id'])
+          fcs = ForeignCourse.where(university_id: @foreign_course.university_id, foreign_course_num: @foreign_course.foreign_course_num, foreign_course_dept: @foreign_course.foreign_course_dept, course_approval_status: false).where.not(id: @foreign_course.id)
+          fcs_ids = fcs.map(&:id)
+          fc_stu = ForeignCoursesStudent.where(foreign_course_id: fcs_ids)
+          for fc in fc_stu do
+            fc.foreign_course_id = @foreign_course.id
+            fc.save
+          end
+          for fc in fcs do
+            fc.destroy
+          end
           @foreign_course_tamu_course.save
         end
 
