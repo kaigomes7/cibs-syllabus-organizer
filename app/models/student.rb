803f5cc7debf3682ba56b08db053340aafa0bdb8
vitruong00 <vimtruong00@gmail.com>
2022-04-14 22:34:48 -0500

    fixed small bug

--- app/models/student.rb
+++ app/models/student.rb
@@ -4,6 +4,7 @@ class Student < ApplicationRecord
   validates :tamu_major, :tamu_college, :classification, presence: true
   belongs_to :tamu_department
   belongs_to :user
+  has_many :foreign_courses_students, dependent: :destroy
   # has_and_belongs_to_many :foreign_course
   def student_name
     User.find(user_id).name.to_s
