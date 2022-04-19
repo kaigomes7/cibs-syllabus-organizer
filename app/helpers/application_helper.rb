7f6da5413f8991590158311f193d4e95a3672d64
Kai Gomes <kaigomes7@tamu.edu>
2022-04-18 20:09:35 -0500

    Fixed flash bootstrap (for real this time)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,11 +4,4 @@ module ApplicationHelper
   def error_messages_for(object)
     render(partial: 'application/error_messages', locals: { object: object })
   end
-
-  def flash_class(level)
-    flash_map = { notice: 'alert alert-info',
-                  success: 'alert alert-success',
-                  alert: 'alert alert-error' }
-    flash_map[level]
-  end
 end
